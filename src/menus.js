8e554aefc9f10d5b7c930b4259e0c8e78ac9f453
ggv582058-ui <ggv582058@gmail.com>
2026-09-07 18:10:24 -0400

    Show cl sticker command in menu

--- src/menus.js
+++ src/menus.js
@@ -2,7 +2,7 @@ import { config } from './config.js'
 import { timeAndDate } from './lib/utils.js'
 
 const categories = {
-  stickers: ['sticker', 's', 'sck', 'cleansticker', 'swm', 'stickerwm', 'toimg', 'stickerinfo'],
+  stickers: ['sticker', 's', 'cl', 'sck', 'cleansticker', 'swm', 'stickerwm', 'toimg', 'stickerinfo'],
   descargas: ['play', 'ytplay', 'youtube.play', 'spotify', 'sp', 'ytmp3', 'yta', 'ytmp4', 'ytvideo', 'playvideo', 'youtube.video', 'tiktok', 'tt', 'tiktoknowm', 'ttnowm', 'instagram', 'ig', 'mediahelp', 'descargas'],
   grupos: ['admins', 'tagall', 'hidetag', 'kick', 'add', 'promote', 'demote', 'link', 'revoke', 'subject', 'desc', 'open', 'close', 'groupinfo'],
   ia: ['ia', 'resumir', 'traducir'],
