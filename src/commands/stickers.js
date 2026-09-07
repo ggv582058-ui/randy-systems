7ccaf8bbe06300dd1051bda81b6d412f6aff7b40
ggv582058-ui <ggv582058@gmail.com>
2026-09-07 18:11:05 -0400

    Fix transparent sticker background

--- src/commands/stickers.js
+++ src/commands/stickers.js
@@ -3,7 +3,7 @@ import { config } from '../config.js'
 
 async function imageToWebp(buffer) {
   return sharp(buffer)
-    .resize(512, 512, { fit: 'contain', background: { r: 0, g: 0, b: 0, a: 0 } })
+    .resize(512, 512, { fit: 'contain', background: { r: 0, g: 0, b: 0, alpha: 0 } })
     .webp({ quality: 82 })
     .toBuffer()
 }
